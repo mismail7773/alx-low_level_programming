eb6e7b55692e908e172815f13fa8703ecd6651cb
Mahmoud Ismail <mi4018284@gmail.com>
2024-01-23 21:18:03 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - draft
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n = '0';
+
+	while (n != 11)
+	{
+		if (n == 58)
+			n = 97;
+		else if (n == 103)
+			n = 10;
+		putchar(n);
+		++n;
+	}
+	return (0);
+}
