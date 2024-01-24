413efe53513b68c8eed23ec407a4c6b7ed89700b
Mahmoud Ismail <mi4018284@gmail.com>
2024-01-24 20:39:33 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - draft
+ */
+void print_alphabet_x10(void)
+{
+	int n = 97, m = 97, o = 0;
+	while (o != 270)
+	{
+		o++;
+		_putchar(n);
+		n++;
+		if (n > 122)
+			n = 10;
+		else if (n == 11)
+			n = m;
+	}
+}
