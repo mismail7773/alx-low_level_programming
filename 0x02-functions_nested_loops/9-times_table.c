8cfe600842385655aabb011331fdd1adaeebe6f5
Mahmoud Ismail <mi4018284@gmail.com>
2024-01-25 16:46:31 +0000

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * times_table - draft
+ */
+void times_table(void)
+{
+	int a = 0, b, c, d;
+
+	while (a != 10)
+	{
+		b = 0;
+		while (b != 10)
+		{
+			c = 0;
+			while (c != 4)
+			{
+				if (a * b > 10)
+				{
+					if (c == 0)
+						d = -16;
+					if (c == 1)
+						d = (a * b) / 10;
+					if (c == 2)
+						d = (a * b) % 10;
+				}
+				else
+				{
+					if (c == 0)
+						d = -16;
+					if (c == 1)
+						d = -16;
+					if (c == 2)
+						d = (a * b) % 10;
+				}
+				if (c == 3 && b == 9)
+					d = -38;
+				else if (c == 3)
+					d = -4;
+				if (b != 0 || (b == 0 && c > 1))
+					_putchar(d + '0');
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
