7e8d5862c8bca839562b4e151ce99db2a9cea001
Mahmoud Ismail <mi4018284@gmail.com>
2024-01-25 21:31:59 +0000

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_to_98 - draft
+ * @n: draft
+ */
+void print_to_98(int n)
+{
+	int a = 0, b, c, d, e;
+
+	while (n != 98 || a == 0)
+	{
+		if (n > 98 && a > 1)
+			n -= 1;
+		if (n < 98 && a > 1)
+			n += 1;
+		c = n;
+		b = 1;
+		while (n / 10 != 0)
+		{
+			b++;
+			n /= 10;
+		}
+		d = 1;
+		e = b;
+		while (--b)
+			d *= 10;
+		n = c;
+		if (n < 0)
+		{
+			n *= -1;
+			_putchar('-');
+		}
+		while (e >= 1)
+		{
+			_putchar('0' + c / d);
+			c %= d;
+			d /= 10;
+			a += e--;
+		}
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+			_putchar('\n');
+	}
+}
