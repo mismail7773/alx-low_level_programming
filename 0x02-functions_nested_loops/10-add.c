cb82bebd9c309fba171850fff91d32ae64fad47e
Mahmoud Ismail <mi4018284@gmail.com>
2024-01-25 22:19:46 +0000

    test

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -9,5 +9,17 @@
  */
 int add(int a, int b)
 {
+	return (_add(a, b));
+}
+
+/**
+ * _add - draft
+ * @a: draft
+ * @b: draft
+ *
+ * Return: draft
+ */
+int _add(int a, int b)
+{
 	return (a + b);
 }
